df1b69a7282de721f3074cfc75384d1766de5041
cdevishree <devishree.gj@gmail.com>
2016-08-06 16:50:35 +0530

    Create Pangram.java

---
+++ Pangram.java
@@ -0,0 +1,40 @@
+package lesum;
+
+import java.util.*;
+
+
+public class Allalpahbet {
+
+    public static final int n = 26; 
+
+    public int check(String arr){
+        if(arr.length() < n){
+           return -1;
+        }
+        for(char c = 'A'; c <= 'Z' ; c++){
+            if((arr.indexOf(c) < 0) && (arr.indexOf((char)(c + 32)) < 0)){
+               return -1;
+            }
+        }
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner s1 = new Scanner(System.in);
+        String s = s1.next();
+        Allalpahbet all = new Allalpahbet();
+
+        int d = all.check(s);
+
+        if(d == -1)
+            System.out.print("the string is not a pangram");
+        else
+             System.out.print("the given string is pangram");
+
+        s1.close();
+    }
+    
+}
+
