2a4b26e0b22650d9410d5e3577ee5928cd9614cd
cdevishree <devishree.gj@gmail.com>
2016-08-07 14:02:35 +0530

    Create WordReverse.java

---
+++ WordReverse.java
@@ -0,0 +1,14 @@
+package lesum;
+
+public class Reverseword {
+
+	
+	    public static void main(String args[]){
+	    String str[] = "He is the one".split(" ");
+	    String finalStr="";
+	        for(int i = str.length-1; i>= 0 ;i--){
+	            finalStr += str[i]+" ";
+	        }
+	        System.out.println(finalStr);
+	    }
+	}
